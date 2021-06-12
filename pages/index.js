a0ad5bbbedea93ee6d2743f147a1fbe10f331a62
yonghunj <rrallvv@gmail.com>
2021-06-12 10:37:12 -0400

    grid system added

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,54 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 import AppLayout from '../components/AppLayout';
 const Home = () => {
 
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }));
+
+  
+  const classes = useStyles();
+
   const dispatch = useDispatch()
   const { mainPosts } = useSelector(state => state.post)
   useEffect(() => {
-    console.log(mainPosts)
-    console.log('table function')
-    console.log('grid added');
-    console.log('feature/grid')
+
+    
   }, [mainPosts])
 
   return (
     <AppLayout>
-    { 
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={6} sm={3}>
+            <Paper className={classes.paper}>xs=6 sm=3</Paper>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Paper className={classes.paper}>xs=6 sm=3</Paper>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Paper className={classes.paper}>xs=6 sm=3</Paper>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Paper className={classes.paper}>xs=6 sm=3</Paper>
+          </Grid>
+        </Grid>
+      </div>
+    {/* { 
       mainPosts?.map( (post)=> <div>{post.title}</div> )
-    }
+    } */}
     </AppLayout>
   );
 };
