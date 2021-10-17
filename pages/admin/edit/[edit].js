59ca083eed5f458be0cb138fb347596b28f981ec
Diane <dianekim120@gmail.com>
2021-10-17 09:23:58 -0400

    Change import statements

--- pages/admin/edit/[edit].js
+++ pages/admin/edit/[edit].js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import axios from 'axios'
 import AppLayout from '../../../components/Layout/AppLayout'
-import TextRichWithNoSSR from "../../../components/textRichWithNoSSR"
+import TextRichWithNoSSR from "../../../components/TextRichWithNoSSR"
 import AdminLayout from '../../../components/Layout/AdminLayout'
 
 const EditPageComponent = () => {
