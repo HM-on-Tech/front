04cd628284d4c28eb0312e0df81bd5b99e828391
Diane <dianekim120@gmail.com>
2021-10-17 12:36:34 -0400

    add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,7 @@ export default class MyDocument extends Document {
       <Html lang='en'>
         <Head>
           <meta name='theme-color' content={theme.palette.primary.main} />
+          <link rel="shortcut icon" href="/static/favicon.png" />
           <link
             rel='stylesheet'
             href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
